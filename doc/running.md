a28d468fdfa25f10ba481398437bc17d2ab30809
TahaKhalili <khaliliahmadtaha@gmail.com>
2025-04-28 20:54:31 -0500

    added running document and changed the server file

---
+++ doc/running.md
@@ -0,0 +1,40 @@
+# Recipe Generator App running instructions
+
+## How to run!
+
+After following the steps listed in development.md, you can navigate this file to see how to run the app and find every feature that it has to offer
+
+## Home page
+
+- After running the command:
+  npm run dev
+
+- You should type:
+  localhost:3500
+
+After that you are presented with the home page.
+
+## Search page
+
+- One of the core features of the app is finding food recipes based on ingredients that users search.
+
+- You should type:
+  localhost:3500/search
+
+This page has three main buttons to navigate other pages in the app. About, Recipes, and Home page buttons.
+
+After clicking search, you will be guided to /searchResult page where the corresponded items are displayed
+
+# Recipes page
+
+- The other feature of the app is displaying all the recipes in the database and having the ability to filter food recipes based on user entry. The filtration system is currently in development.
+
+- You should type:
+  localhost:3500/recipes
+
+## About page
+
+- The About page gives you helpful information about the contributors of the project, the different features that the app offers, and helpful tips on how and why to use the app.
+
+- You should type:
+  localhost:3500/about
--- server.js
+++ server.js
@@ -1,91 +1,3 @@
-// const express = require('express');
-// const app = express();
-// const path = require('path');
-// const PORT = process.env.PORT || 3500
-// const fs = require('fs');
-// const csv = require('csv-parser');
-
-// let recipes = [];
-
-// // Load recipes on server start
-// fs.createReadStream(path.join(__dirname, 'recipes-sample.csv'))
-//   .pipe(csv())
-//   .on('data', (row) => {
-//     if (row['Cleaned_Ingredients']) {
-//       row['Cleaned_Ingredients'] = row['Cleaned_Ingredients'].toLowerCase();
-//     }
-//     recipes.push(row);
-//   })
-//   .on('end', () => {
-//     console.log('CSV loaded.');
-//   });
-
-// // Home page: Search form
-// app.get('/', (req, res) => {
-//   res.send(`
-//     <h2>Recipe Search</h2>
-//     <form method="GET" action="/search">
-//       <input name="ingredient" placeholder="Enter an ingredient..." required />
-//       <button type="submit">Search</button>
-//     </form>
-//     <p><a href="/about">About</a> | <a href="/recipes">All Recipes</a> | <a href="/home"> Home</a></p>
-//   `);
-// });
-
-// // Search route
-// app.get('/search', (req, res) => {
-//   const query = req.query.ingredient?.toLowerCase();
-
-//   if (!query) {
-//     return res.send('Please provide an ingredient to search.');
-//   }
-
-//   const results = recipes.filter(recipe =>
-//     recipe['Cleaned_Ingredients']?.includes(query)
-//   );
-
-//   if (results.length === 0) {
-//     return res.send('No recipes found with that ingredient.');
-//   }
-
-//   const html = results.map(r => `
-//     <h3>${r.Title}</h3>
-//     <p><strong>Ingredients:</strong> ${r.Cleaned_Ingredients}</p>
-//     <p><strong>Instructions:</strong> ${r.Instructions}</p>
-//     <hr/>
-//   `).join('');
-
-//   res.send(`<h2>Results for "${query}"</h2>${html}<p><a href="/">Back to Search</a></p>`);
-// });
-
-// // About page
-// app.use('/', require('./routes/root'))
-
-// // All recipes page
-// app.get('/recipes', (req, res) => {
-//   if (recipes.length === 0) {
-//     return res.send('No recipes available.');
-//   }
-
-//   const html = recipes.map(r => `
-//     <h3>${r.Title}</h3>
-//     <p><strong>Ingredients:</strong> ${r.Cleaned_Ingredients}</p>
-//     <p><strong>Instructions:</strong> ${r.Instructions}</p>
-//     <hr/>
-//   `).join('');
-
-//   res.send(`<h2>All Recipes</h2>${html}<p><a href="/">Back to Search</a></p>`);
-// });
-
-// // Handle all other routes with a 404 error
-// app.use((req, res) => {
-//   res.status(404).send('<h1>404 - Page Not Found</h1><p>This route does not exist on this server.</p><a href="/">Go back home</a>');
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`Server running at http://localhost:${PORT}`);
-// });
-
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -109,7 +21,7 @@ fs.createReadStream(path.join(__dirname, 'recipes-sample.csv'))
   });
 
 // Middleware to serve static HTML files
-app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Home page ('/' and '/home')
 app.get(['/', '/home'], (req, res) => {
@@ -177,7 +89,7 @@ app.get('/recipes', (req, res) => {
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).send('<h1>404 - Page Not Found</h1><p>This route does not exist on this server.</p><a href="/">Go back home</a>');
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 // Start server
